1746477f53f03078450f8f457c9104775b9348c7
ritu279 <ritubhat2794@gmail.com>
2025-06-06 23:57:31 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// Typed.js animation
+const typed = new Typed('.typed-text', {
+  strings: ['Frontend Developer', 'UI/UX Enthusiast', 'React Developer'],
+  typeSpeed: 70,
+  backSpeed: 40,
+  loop: true
+});
